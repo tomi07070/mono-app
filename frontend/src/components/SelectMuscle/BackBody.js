74985a3ac7b68c1e8631d3f8a15dbba6b1f9ddd7
tomi07070 <147639329+tomi07070@users.noreply.github.com>
2024-03-07 17:35:27 -0300

    commit

--- frontend/src/components/SelectMuscle/BackBody.js
+++ frontend/src/components/SelectMuscle/BackBody.js
@@ -1,11 +1,27 @@
 import * as React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, Button } from 'react-native';
+import tw from 'twrnc'
 
 
 export default function BackBody() {
   return (
     <View style={styles.container}>
       <Image source={require('../../../assets/img/hombreEspalda.png')}/>
+      <View style={tw`absolute top-[290px] left-[-60px]`}>
+        <Button title='TRICEPS' color={'#FF8000'}/>
+      </View>
+      <View style={tw`absolute top-[270px] left-[180px]`}>
+        <Button title='ESPALDA' color={'#FF8000'}/>
+      </View>
+      <View style={tw`absolute top-[435px] left-[-50px]`}>
+        <Button title='FEMORAL' color={'#FF8000'}/>
+      </View>
+      <View style={tw`absolute top-[370px] left-[180px]`}>
+        <Button title='GLUTEO' color={'#FF8000'}/>
+      </View>
+      <View style={tw`absolute top-[530px] left-[180px]`}>
+        <Button title='GEMELO' color={'#FF8000'}/>
+      </View>
     </View>
   );
 }
--- frontend/src/components/SelectMuscle/FrontBody.js
+++ frontend/src/components/SelectMuscle/FrontBody.js
@@ -1,11 +1,32 @@
 import * as React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
-
+import { StyleSheet, View, Image, Button } from 'react-native';
+import tw from 'twrnc'
 
 export default function FrontBody() {
   return (
     <View style={styles.container}>
       <Image source={require('../../../assets/img/hombreFrente.png')}/>
+      <View style={tw`absolute top-[240px] left-[-40px]`}>
+        <Button title='HOMBRO' color={'#FF8000'}/>
+      </View>
+      <View style={tw`absolute top-[290px] left-[-20px]`}>
+        <Button title='BICEP' color={'#FF8000'}/>
+      </View>
+      <View style={tw`absolute top-[350px] left-[-60px]`}>
+        <Button title='ANTEBRAZO' color={'#FF8000'}/>
+      </View>
+      <View style={tw`absolute top-[200px] left-[190px]`}>
+        <Button title='PECHO' color={'#FF8000'}/>
+      </View>
+      <View style={tw`absolute top-[440px] left-[-60px]`}>
+        <Button title='CUADRICEPS' color={'#FF8000'}/>
+      </View>
+      <View style={tw`absolute top-[325px] left-[200px]`}>
+        <Button title='ABDOMINALES' color={'#FF8000'}/>
+      </View>
+      <View style={tw`absolute top-[525px] left-[200px]`}>
+        <Button title='SOLEO' color={'#FF8000'}/>
+      </View>
     </View>
   );
 }
