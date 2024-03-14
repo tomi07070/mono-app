d7f6c2a3c94dea65a79dcc1bba08bcf6bf147d59
tomi07070 <147639329+tomi07070@users.noreply.github.com>
2024-03-14 20:10:19 -0300

    commit

--- frontend/src/components/ExerciseCard.js
+++ frontend/src/components/ExerciseCard.js
@@ -1,26 +1,18 @@
 import React from "react";
 import { View, Image, Text } from "react-native";
 import FontAwesome from '@expo/vector-icons/FontAwesome'
-
+import tw from 'twrnc'
 
 export default function ExerciseCard() {
     return (
         <View>
             <Image/>
             <Text></Text>
-            <FontAwesome.Button style={tw``} 
-                    name='plus' 
+            <FontAwesome.Button 
+                    name="check-square-o"
                     backgroundColor={'white'} 
                     color={'orange'} 
                     iconStyle={tw`text-4xl`} 
-                    onPress={() => navigation.navigate('Select Muscle')} 
-                />
-                <FontAwesome.Button style={tw``} 
-                    name='plus' 
-                    backgroundColor={'white'} 
-                    color={'orange'} 
-                    iconStyle={tw`text-4xl`} 
-                    onPress={() => navigation.navigate('Select Muscle')} 
                 />
         </View>
     )
