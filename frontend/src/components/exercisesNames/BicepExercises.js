9eed794f31149fc889754d50e3fd4404c7e21861
tomi07070 <147639329+tomi07070@users.noreply.github.com>
2024-03-17 16:20:02 -0300

    commit

---
+++ frontend/src/components/exercisesNames/BicepExercises.js
@@ -0,0 +1,16 @@
+const exercises = [
+    {name: 'Barbell Curl', image: 'https://fitnessprogramer.com/wp-content/uploads/2021/02/Barbell-Curl.gif'},
+    {name: 'Dumbbell Curl', image: 'https://newlife.com.cy/wp-content/uploads/2019/11/23211301-Dumbbell-Standing-Inner-Biceps-Curl-version-2_Upper-Arms_360.gif'},
+    {name: 'Hammer Curl', image: 'https://www.inspireusafoundation.org/wp-content/uploads/2022/04/dumbbell-hammer-curl.gif'},
+    {name: 'Preacher Curl', image: 'https://www.inspireusafoundation.org/wp-content/uploads/2022/03/ez-bar-preacher-curl.gif'},
+    {name: 'Concentration Curl', image: 'https://fitnessprogramer.com/wp-content/uploads/2021/02/Concentration-Curl.gif'},
+    {name: 'EZ-Bar Curl', image: 'https://newlife.com.cy/wp-content/uploads/2019/11/04461301-EZ-Barbell-Close-grip-Curl_Upper-Arms_360.gif'},
+    {name: 'Cable Curl', image: 'https://fitnessprogramer.com/wp-content/uploads/2022/02/Cable-Reverse-Grip-EZ-bar-Biceps-Curl.gif'},
+    {name: 'Incline Dumbbell Curl', image: 'https://i.pinimg.com/originals/b4/30/64/b43064d9ad9f42493dbcf37e653ab87c.gif'},
+    {name: 'Reverse Curl', image: 'https://fitnessprogramer.com/wp-content/uploads/2021/02/Barbell-Reverse-Curl.gif'},
+    {name: 'Lever Biceps Curl', image: 'https://fitnessprogramer.com/wp-content/uploads/2021/06/Lever-Biceps-Curl.gif'},
+    {name: 'Rope Biceps Curl', image: 'https://cdn-cccio.nitrocdn.com/sQAAylIpwgMYZgBLSXcMgCkUIbfIzHvb/assets/images/optimized/rev-3d9de4c/www.aleanlife.com/wp-content/uploads/2020/08/rope-bicep-curls.gif'},
+    {name: 'Bayesian Curl', image: 'https://www.inspireusafoundation.org/wp-content/uploads/2022/10/bayesian-curl.gif'},
+]
+
+export default exercises
