d374ccce06b4f2827c5f9fbb1e6d4ba6eeb3ecbf
tomi07070 <147639329+tomi07070@users.noreply.github.com>
2024-03-20 20:12:28 -0300

    commit

---
+++ frontend/src/components/exercisesNames/CalvesExercises.js
@@ -0,0 +1,15 @@
+const exercises = [
+    {name: 'Standing Calfs Raises', image: 'https://www.meanmuscles.com/wp-content/uploads/2020/09/13731301-Bodyweight-Standing-Calf-Raise_Calves_360.gif'},
+    {name: 'Standing Barbel Calfs Raises', image: 'https://fitnessprogramer.com/wp-content/uploads/2022/04/Standing-Barbell-Calf-Raise.gif'},
+    {name: 'Standing Dumbbell Calfs Raises', image: 'https://fitnessprogramer.com/wp-content/uploads/2021/02/Dumbbell-Calf-Raise.gif'},
+    {name: 'Machine Seated Calfs Raises', image: 'https://newlife.com.cy/wp-content/uploads/2019/11/26661301-Lever-Seated-Calf-Raise-plate-loaded-VERSION-2_Calves_360.gif'},
+    {name: 'Dumbbell Seated Calfs Raises', image: 'https://i.pinimg.com/originals/fd/19/2b/fd192bb6ef1ff52e0babb696c59500f1.gif'},
+    {name: 'Barbel Seated Calfs Raises', image: 'https://i.pinimg.com/originals/7b/96/51/7b9651f3ad4c05bab422a7fece5e470a.gif'},
+    {name: 'Donkey Calf Raises', image: 'https://www.inspireusafoundation.org/wp-content/uploads/2023/03/donkey-calf-raise.gif'},
+    {name: 'Single-Leg Calf Raises', image: 'https://pitpowerapp.pt/wp-content/uploads/2022/01/476113.gif'},
+    {name: 'Leg Press Calf Raise', image: 'https://www.inspireusafoundation.org/wp-content/uploads/2022/09/leg-press-calf-raise.gif'},
+    {name: 'Smith Machine Calf Raise', image: 'https://genemedicshealth.s3-us-west-2.amazonaws.com/360/11641301-Smith-Calf-Raise-(version-2)_Calves_360.gif'},
+    {name: 'Calf Raises with Resistance Bands', image: 'https://fitnessprogramer.com/wp-content/uploads/2022/05/Calf-Raise-with-Resistance-Band.gif'},
+    {name: 'Calf Press on Hack Squat Machine', image: 'https://i.pinimg.com/originals/93/bb/21/93bb21f90a649a25fc6b7d7ac66cabe0.gif'},
+]
+export default exercises
