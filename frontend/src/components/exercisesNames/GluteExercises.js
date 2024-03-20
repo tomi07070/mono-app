26188b7a79cb6f89871324e86ebc41939169366b
tomi07070 <147639329+tomi07070@users.noreply.github.com>
2024-03-20 19:51:17 -0300

    commit

---
+++ frontend/src/components/exercisesNames/GluteExercises.js
@@ -0,0 +1,11 @@
+const exercises = [
+    {name: 'Barbel Hip Trust', image: 'https://biomecanicadeportiva.cl/wp-content/uploads/2021/04/barbellhipthrust.gif'},
+    {name: 'Cable Pull Through', image: 'https://www.inspireusafoundation.org/wp-content/uploads/2023/07/cable-pull-through.gif'},
+    {name: 'Cable Kickback', image: 'https://www.inspireusafoundation.org/wp-content/uploads/2022/01/cable-kickback.gif'},
+    {name: 'Banded Fire Hydrant', image: 'https://fitnessprogramer.com/wp-content/uploads/2022/02/Banded-Fire-Hydrant.gif'},
+    {name: 'Clamshells', image: 'https://fitnessprogramer.com/wp-content/uploads/2022/02/Band-Side-Lying-Clam.gif'},
+    {name: 'Hip Abduction Machine', image: 'https://newlife.com.cy/wp-content/uploads/2019/11/05971301-Lever-Seated-Hip-Abduction_Hips-FIX_360-1.gif'},
+    {name: 'Reverse Lunges', image: 'https://homeworkouts.org/wp-content/uploads/anim-reverse-lunges.gif'},
+    {name: 'Cable Hip Abduction', image: 'https://newlife.com.cy/wp-content/uploads/2019/12/08791301-Cable-hip-abduction-version-2_Hips_360.gif'},
+]
+export default exercises
