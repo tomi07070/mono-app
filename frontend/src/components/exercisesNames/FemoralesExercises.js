bfeebd034b42f9dffe3e455127322a5541f6db48
tomi07070 <147639329+tomi07070@users.noreply.github.com>
2024-03-21 18:51:27 -0300

    commit

---
+++ frontend/src/components/exercisesNames/FemoralesExercises.js
@@ -0,0 +1,12 @@
+const exercises = [
+    {name: 'Romanian Deadlifts', image: 'https://cdn.shopify.com/s/files/1/0449/8453/3153/files/barbell-deadlift_600x600.gif?v=1690860568'},
+    {name: 'Dumbbell Romanian Deadlifts', image: 'https://www.inspireusafoundation.org/wp-content/uploads/2021/11/dumbbell-romanian-deadlift.gif'},
+    {name: 'Stiff-Leg Deadlifts', image: 'https://fitnessprogramer.com/wp-content/uploads/2022/01/Stiff-Leg-Deadlift.gif'},
+    {name: 'Machine Lying Leg Curls', image: 'https://www.inspireusafoundation.org/wp-content/uploads/2022/04/lying-leg-curl.gif'},
+    {name: 'Seated Leg Curls', image: 'https://fitnessprogramer.com/wp-content/uploads/2021/08/Seated-Leg-Curl.gif'},
+    {name: 'Standing Leg Curls', image: 'https://fitnessprogramer.com/wp-content/uploads/2021/09/Lever-Kneeling-Leg-Curl-.gif'},
+    {name: 'Glute-Ham Raises', image: 'https://modusx.de/wp-content/uploads/glute-ham-raise.gif'},
+    {name: 'Nordic Hamstring Curls', image: 'https://www.kettlebellkings.com/cdn/shop/articles/nordic_curls.gif?v=1699444790&width=1080'},
+    {name: 'Single-Leg Romanian Deadlifts', image: 'https://fitnessprogramer.com/wp-content/uploads/2022/09/Dumbbell-Single-Leg-Deadlift.gif'},
+]
+export default exercises
