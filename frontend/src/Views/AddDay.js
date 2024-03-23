645c4c15794284badf49d7e6c7b1dd43f742fc3e
tomi07070 <147639329+tomi07070@users.noreply.github.com>
2024-03-23 11:42:39 -0300

    commit

--- frontend/src/Views/AddDay.js
+++ frontend/src/Views/AddDay.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {  TextInput, View } from "react-native";
 import FontAwesome from '@expo/vector-icons/FontAwesome'
 import tw from 'twrnc'
 
 export default function AddDay({navigation}) {
     return (
         <>
-            <Text>Add A Day</Text>
             <View>
+                <TextInput
+                    placeholder="Name"
+                />
                 <FontAwesome.Button style={tw``} 
-                    name='plus' 
+                    name='check' 
                     backgroundColor={'white'} 
                     color={'orange'} 
                     iconStyle={tw`text-4xl`} 
