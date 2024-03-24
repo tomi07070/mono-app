5368f48d5077353df8fdb33d609ac486df2f59b5
tomi07070 <147639329+tomi07070@users.noreply.github.com>
2024-03-24 11:37:53 -0300

    commit

---
+++ backend/Models/UserModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+       username: {
+            type: String,
+            required: true,
+       },
+       password: {
+            type: String,
+            required: true,
+            minlength: 6,
+       },
+       gender: {
+            type: String,
+            required: true,
+            enum: ["male", "female"],
+       } 
+    },
+    {timestamps: true}
+)
+
+const User = mongoose.model("User", userSchema)
+
+export default User
