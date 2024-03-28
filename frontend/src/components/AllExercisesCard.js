293534f5a90d63f28da48d9d81bb710c50b78916
tomi07070 <147639329+tomi07070@users.noreply.github.com>
2024-03-28 16:25:52 -0300

    commit

--- frontend/src/components/AllExercisesCard.js
+++ frontend/src/components/AllExercisesCard.js
@@ -1,8 +1,19 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Text, Image } from "react-native";
+import tw from 'twrnc';
 
-export default function AllExercisesCard() {
-    return(
-        <View></View>
+export default function AllExercisesCard({ ExerciseName, ImageUrl }) {
+    return (
+        <View style={tw`flex flex-row bg-white rounded-lg overflow-hidden shadow-md mt-[10px] w-full items-center`}>
+            <Image 
+                style={tw`w-1/4 h-[100px]`}
+                source={{ uri: ImageUrl }} 
+                resizeMode="cover"
+                loadingIndicatorSource={require('../../assets/img/loading.gif')}
+            />
+            <View style={tw`p-4 flex-1`}>
+                <Text style={tw`text-lg font-bold mb-2`}>{ExerciseName}</Text>  
+            </View>
+        </View>
     )
 }
--- frontend/src/components/exercisesNames/AllExercisesNames.js
+++ frontend/src/components/exercisesNames/AllExercisesNames.js
@@ -1,6 +1,28 @@
+import exercises from '../exercisesNames/AbsExercises.js'
+import exercises1 from '../exercisesNames/AntExercises.js'
+import exercises2 from '../exercisesNames/BackExercises.js'
+import exercises3 from '../exercisesNames/BicepExercises.js'
+import exercises4 from '../exercisesNames/CalvesExercises.js'
+import exercises5 from '../exercisesNames/CuadsExercises.js'
+import exercises6 from '../exercisesNames/FemoralesExercises.js'
+import exercises7 from '../exercisesNames/GluteExercises.js'
+import exercises8 from '../exercisesNames/HombroExercises.js'
+import exercises9 from '../exercisesNames/PecExercises.js'
+import exercises10 from '../exercisesNames/TricepsExercises.js'
+
 
 const AllExercisesNames = [
-
+    exercises,
+    exercises1,
+    exercises2,
+    exercises3,
+    exercises4,
+    exercises5,
+    exercises6,
+    exercises7,
+    exercises8,
+    exercises9,
+    exercises10,
 ]
 
 export default AllExercisesNames
